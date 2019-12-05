1aefc7b467f37a8bf2f4f2775c95ce674fb76039
Carlo Baugé <shaqed@outlook.fr>
2019-12-05 22:47:35 +0100

    fix positioning bug

--- calendar.js
+++ calendar.js
@@ -1,12 +1,39 @@
 const fs = require('fs');
 
-let positions = {}
+let positions = []
+
+let width = 1450;
+let height = 900;
+let margin = 20;
+let min_distance = 200; // minimum distance between points
 
 for (let i = 1; i < 26; i++) {
-    positions[i] = {
-        'top': Math.round(Math.random() * (900 - 2 * 50) + 50),
-        'left': Math.round(Math.random() * (1600 - 2 * 50) + 50),
+    let pos = {
+        'top': Math.round(Math.random() * (height - 2 * margin) + margin),
+        'left': Math.round(Math.random() * (width - 2 * margin) + margin),
     }
+    let k = 0;
+    while (!distantEnough(pos, positions, min_distance) && k < 30) {
+        k++;
+        pos = {
+            'top': Math.round(Math.random() * (height - 2 * margin) + margin),
+            'left': Math.round(Math.random() * (width - 2 * margin) + margin),
+        }
+    }
+    if (k >= 30) console.log('failed for ' + i);
+    positions[i] = pos
+}
+
+function distantEnough(p1, point_list, max_dist) {
+    for (let p of point_list) {
+        if (!p || !p.top) continue;
+        if (dist(p1, p) < max_dist) return false;
+    }
+    return true;
+}
+
+function dist(p1, p2) {
+    return Math.sqrt((p1.top - p2.top)**2 + (p1.left - p2.left)**2)
 }
 
 fs.writeFileSync('./positions.js', "const positions = " + JSON.stringify(positions, null, '\t'));
